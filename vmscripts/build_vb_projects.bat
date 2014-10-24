8b49e12f34361b2805b50da3a6fdff1d52846d3c
lenthomas <len@mcs.st-and.ac.uk>
2014-10-24 17:17:27 +0100

    Dumps output into a log file

--- vmscripts/build_vb_projects.bat
+++ vmscripts/build_vb_projects.bat
@@ -1,5 +1,8 @@
 @echo off
+if exist build-distance.log (
+    del build-distance.log
+)
 for /F "tokens=*" %%A in (vbprojectlist.txt) do (
     echo %%A
-    start /wait "VB6" "C:\program files\microsoft visual studio\vb98\vb6.exe" /make %%A
+    start /wait "VB6" "C:\program files\microsoft visual studio\vb98\vb6.exe" /make %%A /out build-distance.log
 )
