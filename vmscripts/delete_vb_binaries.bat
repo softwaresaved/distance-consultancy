b19d029921cbd9334c5b7049c98c70ae585825c4
lenthomas <len@mcs.st-and.ac.uk>
2014-10-29 10:20:06 +0000

    No longer echoes file name before deletion

--- vmscripts/delete_vb_binaries.bat
+++ vmscripts/delete_vb_binaries.bat
@@ -1,9 +1,8 @@
-echo off
+@echo off
 setlocal enabledelayedexpansion
 call set_config.bat
 for /F "tokens=*" %%A in (vbbinarylist.txt) do (
   set FILE=%DISTANCE_BASE%\%%A
   set FILE=!FILE:"=!
-  echo !FILE!
   del "!FILE!"
 )
