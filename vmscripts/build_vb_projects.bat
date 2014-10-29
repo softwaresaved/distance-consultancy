8f90a75b439468badded4a60d3485526df0d25e4
lenthomas <len@mcs.st-and.ac.uk>
2014-10-29 10:20:36 +0000

    Echoes build output immediately rather than at end of run

--- vmscripts/build_vb_projects.bat
+++ vmscripts/build_vb_projects.bat
@@ -1,13 +1,13 @@
-echo off
+@echo off
 setlocal enabledelayedexpansion
 call set_config.bat
-if exist build-distance.log (
-    del build-distance.log
-)
+copy /Y NUL build-distance.log > NUL
 for /F "tokens=*" %%A in (vbprojectlist.txt) do (
   set FILE=%DISTANCE_BASE%\%%A
   set FILE=!FILE:"=!
   echo !FILE!
-  start /wait "VB6" !DISTANCE_VB! /make "!FILE!" /out build-distance.log
+  start /wait "VB6" !DISTANCE_VB! /make "!FILE!" /out build.log
+  type build.log
+  copy build-distance.log+build.log build-distance.log > NUL
+  del build.log
 )
-type build-distance.log
