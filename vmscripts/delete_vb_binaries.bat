956ba9f0a20cea3af3c164d1093092f7f879e2f0
lenthomas <len@mcs.st-and.ac.uk>
2014-10-31 17:25:09 +0000

    Takes list of file prefixes and deletes dll, ocx, tlb, exp, exe, lib

--- vmscripts/delete_vb_binaries.bat
+++ vmscripts/delete_vb_binaries.bat
@@ -4,5 +4,10 @@ call set_config.bat
 for /F "tokens=*" %%A in (vbbinarylist.txt) do (
   set FILE=%DISTANCE_BASE%\%%A
   set FILE=!FILE:"=!
-  del "!FILE!"
+  del "!FILE!.dll" 2> NUL
+  del "!FILE!.ocx" 2> NUL
+  del "!FILE!.lib" 2> NUL
+  del "!FILE!.tlb" 2> NUL
+  del "!FILE!.exp" 2> NUL
+  del "!FILE!.exe" 2> NUL
 )
