86bf0ec8b2f90630998a3c50c345b153e4ab5e9d
mikej888 <m.jackson@software.ac.uk>
2014-12-15 19:36:18 +0000

    Sample makefile

---
+++ makefiles/config.mk
@@ -0,0 +1,12 @@
+# Use Microsoft short paths to avoid problems with spaces
+# From dir /x
+# PROGRA~1     Program Files
+# MICROS~2     Microsoft Visual Studio
+# APPLIC~1     Application Development
+# MI699F~1     Microsoft Office 2003
+
+VB=C:\Progra~1\Micros~2\vb98\vb6.exe
+DISTANCE_BASE=C:\Applic~1\Distance60
+DISTANCE_EXTRAS=C:\Applic~1\Distance60Extras
+DOC_TO_HELP=C:\Progra~1\ComponentOne\DocToHelp\C1D2HBatch.exe
+WINWORD=C:\Progra~1\MI699F~1\OFFICE11\winword.exe
