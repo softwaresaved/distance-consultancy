0de198a8b348dd35be269cafe1b9538c57194d0b
mikej888 <m.jackson@software.ac.uk>
2014-12-17 15:18:21 +0000

    Copies of distance-for-windows files

--- makefiles/config.mk
+++ makefiles/config.mk
@@ -1,12 +1,13 @@
-# Use Microsoft short paths to avoid problems with spaces
-# From dir /x
+# Microsoft short paths avoid problems with spaces (from dir /x)
 # PROGRA~1     Program Files
 # MICROS~2     Microsoft Visual Studio
 # APPLIC~1     Application Development
 # MI699F~1     Microsoft Office 2003
 
 VB=C:\Progra~1\Micros~2\vb98\vb6.exe
-DISTANCE_BASE=C:\Applic~1\Distance60
-DISTANCE_EXTRAS=C:\Applic~1\Distance60Extras
+DF_VARS_BAT=C:\Progra~1\Micros~2\DF98\BIN\DFVARS.BAT
 DOC_TO_HELP=C:\Progra~1\ComponentOne\DocToHelp\C1D2HBatch.exe
 WINWORD=C:\Progra~1\MI699F~1\OFFICE11\winword.exe
+
+DISTANCE_BASE=C:\Applic~1\Distance60
+DISTANCE_EXTRAS=C:\Applic~1\Distance60Extras
